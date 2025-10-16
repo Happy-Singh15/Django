342fe25e310bfe194d8aa1b59317665b7953393b
Happy Singh <solo.leveler15@gmail.com>
2025-10-16 23:12:14 +0530

    printed the data

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,5 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint,json= {"id":1},params={"id":2})
-print(get_response.json())
+print(get_response.text)
 
