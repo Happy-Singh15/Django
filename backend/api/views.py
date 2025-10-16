2cb32d4ff076610cdfa2c102568e707127418455
Happy Singh <solo.leveler15@gmail.com>
2025-10-16 23:11:14 +0530

    get data from database and converted the data into dict for json convesion of data using model_to_dict method of djangoRestFramework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,20 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
 
+from products.models import Products
 def api_home(request, *args, **kwargs):
-    body = request.body
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params']= dict(request.GET)
-    data['headers']= dict(request.headers)
-    data['content_type']= request.content_type
+    model_data = Products.objects.all().order_by('?').first()
+    data={}
+    if model_data:
+        # without using model_to_dict
+
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+
+        # using model_to_dict
+        data = model_to_dict(model_data,fields=['id','title','price'])
     return JsonResponse(data)
 
