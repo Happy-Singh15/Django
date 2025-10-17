37ce3cf154ad9b9988793dea978ea0f1185b22c8
Happy Singh <solo.leveler15@gmail.com>
2025-10-17 15:48:44 +0530

    created a post request

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint,json= {"id":1},params={"id":2})
+get_response = requests.post(endpoint,json= {"content":"hello world"},params={"id":2})
 print(get_response.text)
 
