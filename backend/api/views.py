d3c6feb112687d60afe4d7e87343c2840e37c3cf
Happy Singh <solo.leveler15@gmail.com>
2025-10-17 11:59:10 +0530

    used the rest_framework to create a get method

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,15 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse #--> without using django rest framework
 from django.forms.models import model_to_dict
 import json
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from products.models import Products
+
+# @api_view(['POST']) #--> will throw error {"detail":"Method \"GET\" not allowed."} and status code 405
+# @api_view(['GET','POST']) --> can be used for multiple methods
+@api_view(['GET'])#--> will get the data
 def api_home(request, *args, **kwargs):
     model_data = Products.objects.all().order_by('?').first()
     data={}
@@ -16,5 +23,5 @@ def api_home(request, *args, **kwargs):
 
         # using model_to_dict
         data = model_to_dict(model_data,fields=['id','title','price'])
-    return JsonResponse(data)
+    return Response(data)# --> will use JsonResponse without using django rest framework
 
