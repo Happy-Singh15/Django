00446dc3b11704e0e1104683991017bfd0e5f755
Happy Singh <solo.leveler15@gmail.com>
2025-10-17 23:11:07 +0530

    added price to the fields

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,12 +10,14 @@ class ProductsSerializer(serializers.ModelSerializer):
             'id',
             'title',
             'content',
+            'price',
             'sale_price',
             'my_discount',
         ]
     def get_my_discount(self,obj):
-        try:
-            return obj.get_discount()
-        except:
+        if not hasattr(obj,'id'):
             return None
+        if not isinstance(obj,Products):
+            return None
+        return obj.get_discount()
         
