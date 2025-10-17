8403891c975f6f363e0aabe552021588cb615849
Happy Singh <solo.leveler15@gmail.com>
2025-10-17 23:13:31 +0530

    added product details url path to api url path

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
 # from django.http import JsonResponse #--> without using django rest framework
-from django.forms.models import model_to_dict
-import json
+# from django.forms.models import model_to_dict
+# import json
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from products.models import Products
+# from products.models import Products
 from products.serializers import ProductsSerializer
 
 
