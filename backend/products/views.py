21e5cf1a9a73b79b00294fab0135c53b10796322
Happy Singh <solo.leveler15@gmail.com>
2025-10-17 23:08:44 +0530

    created an api view for details of product

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Products
+from .serializers import ProductsSerializer
+
+class ProductsDetailAPIView(generics.RetrieveAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+product_detail_view = ProductsDetailAPIView.as_view()#--> can also be directly used in urls.py of products as view.ProductsDetailAPIView.as_view instead of product_detail_view
