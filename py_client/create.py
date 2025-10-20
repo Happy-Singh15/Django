8914d07f27b168cdbe22f26a4c0af49e936486f3
Happy Singh <solo.leveler15@gmail.com>
2025-10-21 00:11:34 +0530

    added the token authentication to create and list products

--- py_client/create.py
+++ py_client/create.py
@@ -1,11 +1,27 @@
 import requests
+from getpass import getpass
 
-endpoint = 'http://localhost:8000/api/products/'
+username = input('What is your username?\n')
+password = getpass('What is your password?\n')
+
+auth_endpoint = 'http://localhost:8000/api/auth/'
+
+auth_response = requests.post(auth_endpoint,json={'username':username,'password':password})
+
+if auth_response.status_code == 200:
+
+    token = auth_response.json()['token']
+    headers ={
+        'Authorization':f'Bearer {token}'
+        # 'Authorization':'Bearer 26f371cf252e6d36bbd6ce5c6799b83e155ff548'#--> used raw token to emulate the delete token from admin panel
+        #--> default is Token , Bearer is created inside api authentication.py file
+    }
+    endpoint = 'http://localhost:8000/api/products/'
 
 
-data = {
-    'title':'this is the title'
-}
-create_instance = requests.post(endpoint,json=data)
+    data = {
+        'title':'this is the title'
+    }
+    create_instance = requests.post(endpoint,json=data,headers=headers)
 
-print(create_instance.json())
+    print(create_instance.json())
--- py_client/list.py
+++ py_client/list.py
@@ -1,7 +1,20 @@
 import requests
+from getpass import getpass
 
-endpoint = 'http://localhost:8000/api/products/'
+username = input('What is your username?\n')
+password = getpass('What is your password?\n')
 
+auth_endpoint = 'http://localhost:8000/api/auth/'
 
-list_response = requests.get(endpoint)
-print(list_response.json())
+auth_response = requests.post(auth_endpoint,json={'username':username,'password':password})
+
+if auth_response.status_code == 200:
+
+    token = auth_response.json()['token']
+    headers ={
+        'Authorization':f'Bearer {token}'#--> default is Token , Bearer is created inside api authentication.py file
+    }
+    endpoint = 'http://localhost:8000/api/products/'
+
+    list_response = requests.get(endpoint,headers=headers)
+    print(list_response.json())
