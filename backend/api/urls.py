01b04a31434826f0ba119886a96c805d865e40af
Happy Singh <solo.leveler15@gmail.com>
2025-10-21 00:08:54 +0530

    created a url for obtaining the token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 # from .views import api_home
 
 urlpatterns= [
     #path('',api_home),
+    path('auth/',obtain_auth_token),
     path('products/',include('products.urls'))#--> can also be included in cfehome urls directly with another endpoint 
 ]
