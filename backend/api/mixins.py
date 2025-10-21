04a494b6df3388c5655a4d16624df1dadb532abb
Happy Singh <solo.leveler15@gmail.com>
2025-10-21 22:01:21 +0530

    created a mixin for staff permission

---
+++ backend/api/mixins.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+from .permissions import IsStaffEditorPermission
+
+class StaffPermissionMixin():
+    permission_classes =[
+        permissions.IsAdminUser,
+        IsStaffEditorPermission
+    ]
