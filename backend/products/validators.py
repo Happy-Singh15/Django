7c2f9400674fbee080bbb5f4ead35d1e30c906e9
Happy Singh <solo.leveler15@gmail.com>
2025-10-22 22:31:15 +0530

    updated the unique_product_title

--- backend/products/validators.py
+++ backend/products/validators.py
@@ -4,9 +4,9 @@ from rest_framework.validators import UniqueValidator
 from .models import Products
 
 def validate_title_no_hello(value):
-    qs = Products.objects.filter(title__iexact=value)
+    # qs = Products.objects.filter(title__iexact=value)
     if 'hello' in value.lower():
         raise serializers.ValidationError('hello is not allowed.')
     return value
 
-unique_product_title = UniqueValidator(queryset=Products.objects.all())
+unique_product_title = UniqueValidator(queryset=Products.objects.all(),lookup='iexact')
