b3ba467ab3f85e0b0e7f815246721b30a5c9ff2a
Happy Singh <solo.leveler15@gmail.com>
2025-10-22 21:40:39 +0530

    imported and used the custom validators

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from .models import Products
+from . import validators
 
 class ProductsSerializer(serializers.ModelSerializer):
     my_discount  = serializers.SerializerMethodField(read_only=True)
+    title = serializers.CharField(validators=[
+        validators.validate_title_no_hello,
+        validators.unique_product_title])
     class Meta:
         model = Products
         fields = [
@@ -14,6 +18,7 @@ class ProductsSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
     def get_my_discount(self,obj):
         if not hasattr(obj,'id'):
             return None
